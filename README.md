8301e915ef506414bb62237d2cb4b75f96bc1a2b
dan <dansontan23@gmail.com>
2025-03-05 21:00:54 +0800

    creating raw skeleton of project, main.go as well

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cognizant
 assessment for cognizant
+using net http for routing, simple and easy to use, best suited for this assessment
+
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"e-library/config"
+	"e-library/transport"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.LoadConfig()
+
+	http.HandleFunc("/Book", transport.GetBookHandler)
+	http.HandleFunc("/Borrow", transport.BorrowBookHandler)
+	http.HandleFunc("/Extend", transport.ExtendLoanHandler)
+	http.HandleFunc("/Return", transport.ReturnBookHandler)
+
+	log.Println("Server started on port 3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
